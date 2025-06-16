f08062f86819bbc2eed8b3194f1b631a356a932f
LautaroStude <81266009+LautaroStude@users.noreply.github.com>
2025-06-15 21:27:53 -0300

    Update IM-reentrega.py

--- IM-reentrega.py
+++ IM-reentrega.py
@@ -33,18 +33,21 @@ def seleccionar_canal(imagen, nombre_canal):
 
 def convertir_a_grises(imagen):
     R, G, B = separar_canales(imagen)
-    return 0.299 * R + 0.587 * G + 0.114 * B
+    res = 0.299 * R + 0.587 * G + 0.114 * B
+    return res 
 
 
 def multiplicar_y_sumar(array1, array2):
-    return np.sum(array1 * array2)
+    sum = np.sum(array1 * array2)
+    return sum
 
 
 def aplicar_stencil_a_pos(array, posicion, stencil):
     x, y = posicion
     margen = stencil.shape[0] // 2
     recorte = array[x - margen : x + margen + 1, y - margen : y + margen + 1]
-    return multiplicar_y_sumar(recorte, stencil)
+    res = multiplicar_y_sumar(recorte, stencil)
+    return res
 
 
 def aplicar_stencil(array, stencil):
@@ -62,14 +65,15 @@ def suavizar(imagen):
     stencil = np.array(
         [[1 / 12, 1 / 8, 1 / 12], [1 / 8, 1 / 6, 1 / 8], [1 / 12, 1 / 8, 1 / 12]]
     )
-    return aplicar_stencil(imagen, stencil)
+    res = aplicar_stencil(imagen, stencil)
+    return res
 
 
 def suavizar_k_veces(imagen, k):
     resultado = np.copy(imagen)
-    for _ in range(k):
-        resultado = suavizar(resultado)
-    return resultado
+    for i in range(k):
+        res = suavizar(resultado)
+    return res
 
 
 def combinar_canales(r, g, b):
@@ -87,7 +91,8 @@ def suavizar_color(imagen):
     r_suav = suavizar(R)
     g_suav = suavizar(G)
     b_suav = suavizar(B)
-    return combinar_canales(r_suav, g_suav, b_suav)
+    res = combinar_canales(r_suav, g_suav, b_suav)
+    return res
 
 
 def suavizar_color_k_veces(imagen, k):
@@ -95,8 +100,10 @@ def suavizar_color_k_veces(imagen, k):
     r_suav = suavizar_k_veces(R, k)
     g_suav = suavizar_k_veces(G, k)
     b_suav = suavizar_k_veces(B, k)
-    return combinar_canales(r_suav, g_suav, b_suav)
+    res = combinar_canales(r_suav, g_suav, b_suav)
+    return res
 
 
 def guardar_imagen(imagen, nombre_archivo):
     plt.imsave(nombre_archivo, imagen)
+    return
